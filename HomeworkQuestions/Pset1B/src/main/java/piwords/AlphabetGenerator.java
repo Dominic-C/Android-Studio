a8826bc32307f3c86341b2fc9356ca98b347a888
dominic <dominiccordeiro@gmail.com>
2018-09-24 23:31:24 +0800

    cleanup part f

--- HomeworkQuestions/Pset1B/src/main/java/piwords/AlphabetGenerator.java
+++ HomeworkQuestions/Pset1B/src/main/java/piwords/AlphabetGenerator.java
@@ -3,7 +3,6 @@ package piwords;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class AlphabetGenerator {
