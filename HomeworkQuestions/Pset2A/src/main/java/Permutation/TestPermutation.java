9ceb6a132f69296094824c8033e6dde02d0ad8fa
dominic <dominiccordeiro@gmail.com>
2018-10-19 22:37:41 +0800

    new attempt at permutation

---
+++ HomeworkQuestions/Pset2A/src/main/java/Permutation/TestPermutation.java
@@ -0,0 +1,17 @@
+package Permutation;
+
+import java.util.ArrayList;
+
+public class TestPermutation  {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> v;
+        Permutation  p  = new Permutation("hate");
+
+        p.permute();
+        v  = p.getA();
+
+        System.out.println(v);
+    }
+}
